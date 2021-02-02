3b0ed7603118f7940940dee3954f7bbd0157f02f
Kaue Salem <magidsalem96@gmail.com>
2021-02-02 19:18:44 -0300

    List View

--- app/src/main/java/com/kauemagid/lists/MainActivity.java
+++ app/src/main/java/com/kauemagid/lists/MainActivity.java
@@ -3,12 +3,45 @@ package com.kauemagid.lists;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private String[] itens = {
+        "Angra", "São Paulo", "Rio de Janeiro", "Mexico"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.ListAdress);
+
+        //Create adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+        );
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String itemSelected = listView.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), itemSelected, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
